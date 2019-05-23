c49c17265cd4622afdaf072e17f94f854ebf6d59
Jiayi Wang <clairejw1015@gmail.com>
2019-05-23 23:58:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+  new_array = [ ]
+  i = 0
+  while i < array.length do
+    first = array[i].split(" ").first
+    new_array << yield(first)
+    i += 1
+  end
+  new_array
+end
 
